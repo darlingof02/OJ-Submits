b44c445026aa30d25ea90a5fc890ea8bb61c6ab0
stdKonjac <918836143@qq.com>
2018-09-07 12:39:26 +0800

    2018.9.7

--- HDU/ACM Steps/Chapter 2/Section 1/2.1.4.cpp
+++ HDU/ACM Steps/Chapter 2/Section 1/2.1.4.cpp
@@ -7,12 +7,17 @@
 #include<algorithm>
 using namespace std;
 int p,q;
+int gcd(int a,int b)
+{
+    return (!b)?a:gcd(b,a%b);
+}
 int main()
 {
     freopen("test.in","r",stdin);
     freopen("test.out","w",stdout);
     while(~scanf("%d %d",&p,&q))
     {
-        
+        cout<<p+q-gcd(p,q)<<endl;
     }
+    return 0;
 }
