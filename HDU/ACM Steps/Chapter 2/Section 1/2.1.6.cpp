3e14432bdf77fa55b6e72540700859989f587103
stdKonjac <918836143@qq.com>
2018-09-08 11:08:08 +0800

    2018.9.8

--- HDU/ACM Steps/Chapter 2/Section 1/2.1.6.cpp
+++ HDU/ACM Steps/Chapter 2/Section 1/2.1.6.cpp
@@ -8,19 +8,78 @@
 using namespace std;
 int T;
 int n;
-int gcd(int a,int b)
+
+int k[35000];
+int prime[35000];
+bool flag[35000];
+int cnt;
+
+void make_prime()
 {
-    return (!b)?a:gcd(b,a%b);
+    for(int i=2;i<35000;i++)
+    {
+        if(!flag[i])
+        {
+            prime[++cnt]=i;
+        }
+        for(int j=1;j<=cnt&&i*prime[j]<35000;j++)
+        {
+            flag[i*prime[j]]=true;
+            if(i%prime[j]==0) break;
+        }
+    }
+}
+
+int ksm(int a,int b)
+{
+    int ans=1;
+    while(b)
+    {
+        if(b%2) ans*=a;
+        b>>=1;
+        a*=a;
+    }
+    return ans;
+}
+
+int phi(int x)
+{
+    if(x==1) return 1;
+    memset(k,0,sizeof(k));
+    int pos=1;
+    while(x>=prime[pos])
+    {
+        if(x%prime[pos]==0)
+        {
+            k[pos]++;
+            x/=prime[pos];
+        }
+        else
+        {
+            pos++;
+        }
+    }
+
+    int ans=1;
+    for(int i=1;i<=pos;i++)
+    {
+        if(k[i]>=1)
+        {
+            ans*=ksm(prime[i],k[i]-1)*(prime[i]-1);
+        }
+    }
+    return ans;
 }
 int main()
 {
     freopen("test.in","r",stdin);
     freopen("test.out","w",stdout);
     scanf("%d",&T);
+    make_prime();
     while(T--)
     {
         scanf("%d",&n);
-        
+        cout<<phi(n)<<endl;
     }
     return 0;
 }
