e52816b53b22f53c2e4914d3dd028e1be2e2694d
stdKonjac <918836143@qq.com>
2018-09-15 20:20:48 +0800

    2018.9.15

---
+++ HDU/ACM Steps/Chapter 2/Section 3/2.3.7.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<cstdio>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+class BigInt
+{
+    
+}
+int n;
+int main()
+{
+    freopen("test.in","r",stdin);
+    freopen("test.out","w",stdout);
+    while(~scanf("%d",&n))
+    {
+        if(n==-1) break;
+        
+    }
+    return 0;
+}
