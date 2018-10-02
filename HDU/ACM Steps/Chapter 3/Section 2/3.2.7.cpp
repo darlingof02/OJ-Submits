d8e5c99f44a1a2b77ddca3699d974e2d91d99d34
stdKonjac <918836143@qq.com>
2018-10-02 13:46:03 +0800

    2018.10.2

--- HDU/ACM Steps/Chapter 3/Section 2/3.2.7.cpp
+++ HDU/ACM Steps/Chapter 3/Section 2/3.2.7.cpp
@@ -7,23 +7,47 @@
 #include<ctime>
 #include<algorithm>
 using namespace std;
-struct item
-{
-    int pos;
-    int time;
-}
-item food[100001];
-int dp[100001];
+int n;
+int map[100010][20];
+int dp[100010][20];
 int main()
 {
     freopen("test.in","r",stdin);
     freopen("test.out","w",stdout);
-    while(~scand("%d",&n))
+    while(~scanf("%d",&n))
     {
         if(!n) break;
+        int maxT=0;
+        memset(map,0,sizeof(map));
+        memset(dp,0,sizeof(dp));
         for(int i=1;i<=n;i++)
         {
-            scanf("%d %d",&food[i].pos,&food[i].time);
+            int x,T;
+            scanf("%d %d",&x,&T);
+            map[T][x]++;
+            maxT=max(maxT,T);
         }
+        for(int i=maxT;i>=0;i--)
+            for(int j=0;j<=10;j++)
+            {
+                if(j==0) 
+                {
+                    dp[i][j]=max(dp[i][j]+map[i][j],dp[i+1][j+1]+map[i][j]);
+                    dp[i][j]=max(dp[i][j],dp[i+1][j]+map[i][j]);
+                }
+                else if(j==10) 
+                {
+                    dp[i][j]=max(dp[i][j]+map[i][j],dp[i+1][j-1]+map[i][j]);
+                    dp[i][j]=max(dp[i][j],dp[i+1][j]+map[i][j]);
+                }
+                else 
+                {
+                    dp[i][j]=max(dp[i][j],dp[i+1][j-1]+map[i][j]);
+                    dp[i][j]=max(dp[i][j],dp[i+1][j+1]+map[i][j]);
+                    dp[i][j]=max(dp[i][j],dp[i+1][j]+map[i][j]);
+                }
+            }
+        cout<<dp[0][5]<<endl;
     }
+    return 0;
 }
